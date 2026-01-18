5c1d379948d3920d4a7416f24ff916c508c58e5c
charlespers <charlespers.m@gmail.com>
2026-01-18 07:11:49 -0600

    Switch to PostgreSQL migrations

--- frontend/prisma/migrations/20260118131129_init_postgres/migration.sql
+++ frontend/prisma/migrations/20260118131129_init_postgres/migration.sql
@@ -1,14 +1,16 @@
 -- CreateTable
 CREATE TABLE "articles" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
     "slug" TEXT NOT NULL,
     "content" TEXT NOT NULL,
     "description" TEXT,
     "tags" TEXT,
-    "published_at" DATETIME,
-    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" DATETIME NOT NULL
+    "published_at" TIMESTAMP(3),
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "articles_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
